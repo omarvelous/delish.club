e7a86edd63fb6e79bf3c7bbb4c2c189c9c5233bf
Omar Johnson <admin@omarvelous.com>
2024-12-01 02:44:48 -0500

    more sprucage

--- app/routes/_app.recipes._index.tsx
+++ app/routes/_app.recipes._index.tsx
@@ -30,96 +30,128 @@ export default function Recipes() {
           <aside className="column is-one-third">
             <div className="mb-4">
               <h4>Search</h4>
-              <input
-                className="input"
-                type="search"
-                placeholder="By name or ingredients"
-              />
+              <div className="field has-addons">
+                <div className="control has-icons-left is-expanded">
+                  <input
+                    className="input"
+                    type="search"
+                    placeholder="By name or ingredients"
+                  />
+                  <span className="icon is-small is-left">
+                    <i
+                      aria-hidden="true"
+                      className="fa-solid fa-magnifying-glass"
+                    ></i>
+                  </span>
+                </div>
+                <div className="control">
+                  <button className="button is-link">Search</button>
+                </div>
+              </div>
             </div>
             <hr />
-
-            <div className="mb-4">
-              <h5>Sort By</h5>
-              <div className="select">
-                <select>
-                  <option>Select dropdown</option>
-                  <option>With options</option>
-                </select>
+            <div className="is-hidden-mobile">
+              <h4>Filter</h4>
+              <div className="field mb-4">
+                <label className="label">Sort By</label>
+                <p className="control has-icons-left">
+                  <span className="select">
+                    <select>
+                      <option>Select dropdown</option>
+                      <option>With options</option>
+                    </select>
+                  </span>
+                  <span className="icon is-small is-left">
+                    <i aria-hidden="true" className="fa-solid fa-sort"></i>
+                  </span>
+                </p>
               </div>
-            </div>
-
-            <div className="mb-4">
-              <h5>Category</h5>
-              <div className="select">
-                <select>
-                  <option>Select dropdown</option>
-                  <option>With options</option>
-                </select>
+              <div className="field mb-4">
+                <label className="label">Category</label>
+                <p className="control has-icons-left">
+                  <span className="select">
+                    <select>
+                      <option>Select dropdown</option>
+                      <option>With options</option>
+                    </select>
+                  </span>
+                  <span className="icon is-small is-left">
+                    <i aria-hidden="true" className="fa-solid fa-tags"></i>
+                  </span>
+                </p>
               </div>
-            </div>
-
-            <div className="mb-4">
-              <h5>Cuisine</h5>
-              <div className="select">
-                <select>
-                  <option>Select dropdown</option>
-                  <option>With options</option>
-                </select>
+              <div className="field mb-4">
+                <label className="label">Cuisine</label>
+                <p className="control has-icons-left">
+                  <span className="select">
+                    <select>
+                      <option>Select dropdown</option>
+                      <option>With options</option>
+                    </select>
+                  </span>
+                  <span className="icon is-small is-left">
+                    <i
+                      aria-hidden="true"
+                      className="fa-solid fa-earth-africa"
+                    ></i>
+                  </span>
+                </p>
               </div>
-            </div>
-
-            <div className="mb-4">
-              <h5>Time</h5>
-            </div>
-
-            <div className="mb-4">
-              <h5>Rating</h5>
-              <div className="radios is-flex is-flex-direction-column">
-                <div>
-                  <label className="radio">
-                    <input type="radio" name="rating" value="5" />
-                    <span className="has-text-warning ml-1">
-                      <Stars rating={5} />
-                    </span>
-                  </label>
-                </div>
-                <div>
-                  <label className="radio">
-                    <input type="radio" name="rating" value="4" />
-                    <span className="has-text-warning ml-1">
-                      <Stars rating={4} />
-                    </span>
-                  </label>
-                </div>
-                <div>
-                  <label className="radio">
-                    <input type="radio" name="rating" value="3" />
-                    <span className="has-text-warning ml-1">
-                      <Stars rating={3} />
-                    </span>
-                  </label>
-                </div>
-                <div>
-                  <label className="radio">
-                    <input type="radio" name="rating" value="2" />
-                    <span className="has-text-warning ml-1">
-                      <Stars rating={2} />
-                    </span>
-                  </label>
-                </div>
-                <div>
-                  <label className="radio">
-                    <input type="radio" name="rating" value={1} />
-                    <span className="has-text-warning ml-1">
-                      <Stars rating={1} />
-                    </span>
-                  </label>
+              <div className="field mb-4">
+                <label className="label">Time</label>
+              </div>
+              <div className="filed mb-4">
+                <label className="label">Rating</label>
+                <div className="radios is-flex is-flex-direction-column">
+                  <div>
+                    <label className="radio">
+                      <input type="radio" name="rating" value="5" />
+                      <span className="has-text-warning ml-1">
+                        <Stars rating={5} />
+                      </span>
+                    </label>
+                  </div>
+                  <div>
+                    <label className="radio">
+                      <input type="radio" name="rating" value="4" />
+                      <span className="has-text-warning ml-1">
+                        <Stars rating={4} />
+                      </span>
+                    </label>
+                  </div>
+                  <div>
+                    <label className="radio">
+                      <input type="radio" name="rating" value="3" />
+                      <span className="has-text-warning ml-1">
+                        <Stars rating={3} />
+                      </span>
+                    </label>
+                  </div>
+                  <div>
+                    <label className="radio">
+                      <input type="radio" name="rating" value="2" />
+                      <span className="has-text-warning ml-1">
+                        <Stars rating={2} />
+                      </span>
+                    </label>
+                  </div>
+                  <div>
+                    <label className="radio">
+                      <input type="radio" name="rating" value={1} />
+                      <span className="has-text-warning ml-1">
+                        <Stars rating={1} />
+                      </span>
+                    </label>
+                  </div>
                 </div>
               </div>
-            </div>
-
-            <div className="mb-4">
-              <h5>Difficulty</h5>
+              <div className="field mb-4">
+                <label className="label">Difficulty</label>
+              </div>
+              <div className="buttons">
+                <button className="button is-primary">Apply filters</button>
+                <button className="button is-light">Clear all</button>
+              </div>
             </div>
           </aside>
           <main className="column">
